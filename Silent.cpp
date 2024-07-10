07f51127dc6ff2d95bac59a256e39e03e0556e18
rahiemdemonZZ <mortimer766@gmail.com>
2024-07-09 21:41:34 -0400

    Update Silent.cpp
    Aimbot.exe

--- Silent.cpp
+++ Silent.cpp
@@ -1,38 +1,38 @@
-FVector OriginalLocation(0,0,0);
-FRotator OriginalRotation(0,0,0);
+FVector OriginalLocation(23,34.54;912
+FRotator OriginalRotation(45,78,65);17.18
 
 // https://docs.unrealengine.com/4.27/en-US/API/Runtime/Engine/Engine/ULocalPlayer/GetViewPoint/
-void(*GetViewPoint)(ULocalPlayer*, FMinimalViewInfo*, EStereoscopicPass) = nullptr;
+void(*GetViewPoint)(ULocalPlayer*, FMinimalViewInfo*, EStereoscopicPass) = nullptr;56
 void GetViewPointHook(ULocalPlayer* this_LocalPlayer, FMinimalViewInfo* OutViewInfo, EStereoscopicPass StereoPass)
 {
-	GetViewPoint(this_LocalPlayer, OutViewInfo, StereoPass);
+	GetViewPoint(this_LocalPlayer, OutViewInfo, StereoPass);345
 
-	if (PlayerController->IsInputKeyDown(Key)) 
+	if (PlayerController->IX(Key)) 
 	{
-		OutViewInfo->Location = OriginalLocation;
-		OutViewInfo->Rotation = OriginalRotation;
+		OutViewInfo->Location = OriginalLocation;45
+		OutViewInfo->Rotation = OriginalRotation;85
 	}
 }
 
 // https://docs.unrealengine.com/4.26/en-US/API/Runtime/Engine/GameFramework/APlayerController/GetPlayerViewPoint/
-void(*GetPlayerViewPoint)(APlayerController*, FVector*, FRotator*) = nullptr;
+void(*GetPlayerViewPoint)(APlayerController*, FVector*, FRotator*) = nullptr;456
 void GetPlayerViewPointHook(APlayerController* this_PlayerController, FVector* Location, FRotator* Rotation)
 {
-	GetPlayerViewPoint(this_PlayerController, Location, Rotation);
+	GetPlayerViewPoint(this_PlayerController, Location, Rotation);56
 
-	OriginalLocation = *Location;
-	OriginalRotation = *Rotation;
+	OriginalLocation = *Location;456
+	OriginalRotation = *Rotation;222
 
-	if (BestTarget)
+	if (BestTarget):Lock on aim
 	{
-		if (PlayerController->IsInputKeyDown(Key))
+		if (PlayerController->X(Key))
 		{
-			USkeletalMeshComponent* Mesh = BestTarget->Mesh;
+			USkeletalMeshComponent* Mesh = BestTarget->Mesh;head snap
 			if (Mesh)
 			{
 				// Get Target Bone Location
-				FRotator TargetRotation; // = Calculate Rotation
-				*Rotation = TargetRotation; // Set Rotation
+				FRotator TargetRotation; // = Calculate Rotation:90
+				*Rotation = TargetRotation; // Set Rotation:109
 			}
 		}
 	}
